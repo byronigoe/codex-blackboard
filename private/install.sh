c250ab83a86e5f3139386f8c1298ef777120e394
Dan Rosart <Torgen@users.noreply.github.com>
2021-07-13 22:01:20 -0700

    Update to Meteor 2.3.2 (#382)

--- private/install.sh
+++ private/install.sh
@@ -29,7 +29,7 @@ curl https://install.meteor.com/ | sh
 sudo apt-get install gnupg
 wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
-curl -sL https://deb.nodesource.com/setup_12.x | sudo bash -
+curl -sL https://deb.nodesource.com/setup_14.x | sudo bash -
 sudo apt-get update
 sudo apt-get install -y mongodb-org nodejs software-properties-common
 
