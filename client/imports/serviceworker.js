5f7f07c38539cb5bb18ee937bb7e35e8145c1ed8
Dan Rosart <Torgen@users.noreply.github.com>
2023-05-30 19:24:58 -0700

    Support Firefox private browsing mode (#960)
    window.navigator doesn't have a serviceWorker member in Firefox private
    browsing mode, so don't crash on startup.

--- client/imports/serviceworker.js
+++ client/imports/serviceworker.js
@@ -1,3 +1,3 @@
-export const registrationPromise = navigator.serviceWorker.register(
-  Meteor._relativeToSiteRootUrl("sw.js")
-);
+export const registrationPromise =
+  navigator.serviceWorker?.register(Meteor._relativeToSiteRootUrl("sw.js")) ??
+  Promise.reject("navigator.serviceWorker is absent");
